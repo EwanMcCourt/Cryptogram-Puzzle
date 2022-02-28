3524b91418f10615f07c7793f4c7d579672f3289
Euan Mcallister <euan.mcallister.2020@uni.strath.ac.uk>
2022-02-28 17:25:06 +0000

    working generate cryptogram function

--- src/Cryptogram.java
+++ src/Cryptogram.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Cryptogram {
+
+    static ArrayList<String> phrases = new ArrayList<String>();
+
+
+
+    static String callPhrase(){
+        phrases.add("the quick brown fox jumped over the lazy dog");
+        phrases.add("hi this is lots of is");
+        phrases.add("abcdefghijklmnopqrstuvwxyz");
+
+        Random rand = new Random();
+        int random = rand.nextInt(phrases.size());
+
+
+        return phrases.get(random);
+    }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,40 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.HashMap;
 public class Game {
+    public static void main(String[] args) {
+    String Encrypted = generateCryptogram(Cryptogram.callPhrase());
+
+        //System.out.println("Encrypted = " +Encrypted); //Uncomment to see encrypted phrase
+
+    }
+
+
+    static String generateCryptogram(String phrase){
+        char[] charArray; // used to store the phrase as a char array and used in loops
+        int current; //the ascii value that is eventually shifted
+        Random rand = new Random();
+       int shift = rand.nextInt(1, 26); // a int that the phrase will be shifted by
+        charArray = phrase.toCharArray();
+        char[] result = new char[charArray.length]; // a char array of the encrypted string
+
+        for(int i =0; i< charArray.length; i++){
+            current = phrase.charAt(i); //sets the current ascii value
+            if (phrase.charAt(i) == ' '){  //checking for a space so it can just return a space
+                result[i] = (char) current;
+            } else{
+            current = (current + shift);
+            if (current > 122){ //checks for ascii values past 122 (z) and then wraps them round
+            current = current - 26;
+            }
+            result[i] = (char) current;}}
+
+        //System.out.println("phrase going in = " +phrase); //Uncomment to see unencrypted phrase
+      return new String(result);
+    }
+
+
+
+
 }
