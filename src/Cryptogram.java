7ea0495655ad06c227534d0a3060a2f028baba83
Euan Mcallister <euan.mcallister.2020@uni.strath.ac.uk>
2022-02-28 21:41:13 +0000

    Revamped version of generate cryptogram and new method getfrquencies

--- src/Cryptogram.java
+++ src/Cryptogram.java
@@ -1,21 +1,34 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.HashMap;
 
 public class Cryptogram {
+String phrase;
+HashMap<Character, Character> cryptogramAlphabet = new HashMap<Character, Character>();
+void printDetails(){
+    System.out.println("phrase is " + phrase);
+    for (Character i : cryptogramAlphabet.keySet()) {
+        System.out.println("key: " + i + " value: " + cryptogramAlphabet.get(i));
+    }}
+HashMap<Character, Integer> getFrequencies(){
+    HashMap<Character, Integer>  frequencies = new HashMap<Character, Integer>();
+    char[] charArray = phrase.toCharArray();
+    for (int i=0; i < charArray.length; i++){
+        if (phrase.charAt(i) == ' '){
+            continue;}else{
+        frequencies.put(phrase.charAt(i), 0);}}
+    for (int i=0; i < charArray.length; i++){
+        if (phrase.charAt(i) == ' '){
+            continue;
+        }else{
+            int total = frequencies.get(phrase.charAt(i));
+            frequencies.put(phrase.charAt(i), total +1);}
 
-    static ArrayList<String> phrases = new ArrayList<String>();
 
 
-
-    static String callPhrase(){
-        phrases.add("the quick brown fox jumped over the lazy dog");
-        phrases.add("hi this is lots of is");
-        phrases.add("abcdefghijklmnopqrstuvwxyz");
-
-        Random rand = new Random();
-        int random = rand.nextInt(phrases.size());
-
-
-        return phrases.get(random);
     }
+    return frequencies;
+    }
+
+
 }
--- src/Game.java
+++ src/Game.java
@@ -1,35 +1,58 @@
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.HashMap;
 
 public class Game {
     public static void main(String[] args) {
-    String Encrypted = generateCryptogram(Cryptogram.callPhrase());
+    Cryptogram Encrypted = generateCryptogram();
+    Encrypted.printDetails();
+    System.out.println(Encrypted.getFrequencies());
+    }
 
-        //System.out.println("Encrypted = " +Encrypted); //Uncomment to see encrypted phrase
+    static ArrayList<String> phrases = new ArrayList<String>();
 
+
+
+    static String callPhrase(){
+        phrases.add("the quick brown fox jumped over the lazy dog");
+        phrases.add("hi this is lots of is");
+        phrases.add("abcdefghijklmnopqrstuvwxyz");
+
+        Random rand = new Random();
+        int random = rand.nextInt(phrases.size());
+
+
+        return phrases.get(random);
     }
 
 
-    static String generateCryptogram(String phrase){
+
+    static Cryptogram generateCryptogram(){
+        String phrase = callPhrase();
+
         char[] charArray; // used to store the phrase as a char array and used in loops
         int current; //the ascii value that is eventually shifted
         Random rand = new Random();
        int shift = rand.nextInt(1, 26); // a int that the phrase will be shifted by
         charArray = phrase.toCharArray();
-        char[] result = new char[charArray.length]; // a char array of the encrypted string
+        Cryptogram generated = new Cryptogram();
+        HashMap<Character, Character> cryptogramAlphabet = new HashMap<Character, Character>();
+        generated.phrase = phrase;
 
         for(int i =0; i< charArray.length; i++){
             current = phrase.charAt(i); //sets the current ascii value
-            if (phrase.charAt(i) == ' '){  //checking for a space so it can just return a space
-                result[i] = (char) current;
+            if (phrase.charAt(i) == ' '){  //checking for a space and skips this iteration of the loop
+                continue;
             } else{
             current = (current + shift);
             if (current > 122){ //checks for ascii values past 122 (z) and then wraps them round
             current = current - 26;
             }
-            result[i] = (char) current;}}
-
+            Character original = phrase.charAt(i);
+            cryptogramAlphabet.put(original, (char) current);}}
+            generated.cryptogramAlphabet = cryptogramAlphabet;
         //System.out.println("phrase going in = " +phrase); //Uncomment to see unencrypted phrase
-      return new String(result);
+      return generated;
     }
 
 
