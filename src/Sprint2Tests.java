31dc8e78fe98c2ef51baeef6e62e1cc37e03bc47
tobyweir3 <BreakAt32>
2022-03-20 20:29:00 +0000

    4 tests 2 failing

---
+++ src/Sprint2Tests.java
@@ -0,0 +1,81 @@
+import org.junit.Assert;
+import org.junit.Test;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+import static org.junit.Assert.*;
+
+public class Sprint2Tests {
+    @Test
+    public void TestSaveToFile() throws IOException {
+        Player player = new Player(1, "hardCoded", 0.0, 0, 0, 0, 0);
+        Game game = new Game(player, " ", "./src/phrases.txt");
+        System.setIn(System.in);
+        String simulatedUserInput = "yes" + System.getProperty("line.separator");
+
+        System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes(StandardCharsets.UTF_8)));
+        System.setIn(System.in);
+        game.saveGame();
+        File file = new File("SaveGames.txt");
+        Assert.assertTrue(file.length() > 0);
+
+
+    }
+    /*
+    @Test
+    public void TestLoad() throws IOException {
+        Player player = new Player(1, "hardCoded", 0.0, 0, 0, 0, 0);
+        Game game = new Game(player, " ", "./src/phrases.txt");
+        Cryptogram encrypted = game.getEncrypted();
+        String simulatedUserInput = encrypted.fullEncrypt.get(0) + System.getProperty("line.separator")
+                + "e" + System.getProperty("line.separator");
+
+        System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes(StandardCharsets.UTF_8)));
+        game.enterLetter();
+        System.setIn(System.in);
+        String simulatedUserInput2 = "yes" + System.getProperty("line.separator");
+
+        System.setIn(new ByteArrayInputStream(simulatedUserInput2.getBytes(StandardCharsets.UTF_8)));
+        System.setIn(System.in);
+        game.saveGame();
+        Game game2 = new Game(player);
+        game2.loadGame();
+        assertEquals(game.getEncrypted().guesses,game2.getEncrypted().guesses);
+    }
+    */
+
+    @Test
+    public void TestUserName() throws IOException {
+        Player player = new Player(1, "hardCoded", 0.0, 0, 0, 0, 0);
+        Game game = new Game(player, " ", "./src/phrases.txt");
+        String pUserName = "player";
+        player.setUsername(pUserName);
+        assertEquals(pUserName,player.getUsername());
+    }
+    /*@Test
+    public void TestWins() throws IOException {
+        Player testPlayer = new Player(1, "hardCoded", 0.0, 0, 0, 0, 0);
+        Game game = new Game(testPlayer, "letter", "./src/test.txt");
+        Cryptogram encrypted = game.getEncrypted();
+        for (int i = 0; i < (encrypted.phrase.length()); i++) {
+            InputStream savedStandardInputStream = System.in;
+            if (!(encrypted.phrase.charAt(i) == ' ')) {
+                String simulatedUserInput = encrypted.fullEncrypt.get(i) + System.getProperty("line.separator")
+                        + encrypted.phrase.charAt(i) + System.getProperty("line.separator");
+
+                System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes(StandardCharsets.UTF_8)));
+                encrypted.guesses = game.enterLetter();
+                System.setIn(savedStandardInputStream);
+                game.currentSol();
+            }
+
+
+        }
+    }
+*/
+}
