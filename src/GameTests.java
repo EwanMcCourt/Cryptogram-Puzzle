8a29df30bd3a7b9303c4d3de450a964fee624081
Toby Weir CS2020 <toby.weir.2020@uni.strath.ac.uk>
2022-03-21 07:25:34 +0000

    added tests

--- src/GameTests.java
+++ src/GameTests.java
@@ -253,4 +253,70 @@ public class GameTests {
         Cryptogram currEncrypted2 = game.getEncrypted();
         assertEquals(currEncrypted,currEncrypted2);
     }
+    @Test
+    public void TestLoad() throws IOException {
+        Player player = new Player(1, "hardCoded", 0.0, 0, 0, 0, 0);
+        Game game = new Game(player, " ", "./src/phrases.txt");
+        Cryptogram encrypted = game.getEncrypted();
+        String simulatedUserInput = encrypted.fullEncrypt.get(0) + System.getProperty("line.separator")
+                + "e" + System.getProperty("line.separator");
+
+        System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes(StandardCharsets.UTF_8)));
+        game.enterLetter();
+        System.setIn(System.in);
+        String simulatedUserInput2 = "yes" + System.getProperty("line.separator");
+
+        System.setIn(new ByteArrayInputStream(simulatedUserInput2.getBytes(StandardCharsets.UTF_8)));
+        System.setIn(System.in);
+        game.saveGame();
+        Game game2 = new Game(player);
+        game2.loadGame();
+        assertEquals(game.getEncrypted().fullEncrypt,game2.getEncrypted().fullEncrypt);
+    }
+
+
+    @Test
+    public void TestUserName() throws IOException {
+        Player player = new Player(1, "hardCoded", 0.0, 0, 0, 0, 0);
+        Game game = new Game(player, " ", "./src/phrases.txt");
+        String pUserName = "player";
+        player.setUsername(pUserName);
+        assertEquals(pUserName,player.getUsername());
+    }
+    @Test
+    public void TestStats() throws IOException {
+        Player testPlayer = new Player(1, "hardCoded", 0.0, 0, 0, 0, 0);
+        Game game = new Game(testPlayer, "letter", "./src/test.txt");
+        Cryptogram encrypted = game.getEncrypted();
+        String done = "";
+        for (int i = 0; i<(encrypted.phrase.length()); i++) {
+            InputStream savedStandardInputStream = System.in;
+            if (!(encrypted.phrase.charAt(i) == ' ')&&!done.contains(Character.toString(encrypted.phrase.charAt(i)))) {
+                done = done+encrypted.phrase.charAt(i);
+                String simulatedUserInput = encrypted.fullEncrypt.get(i) + System.getProperty("line.separator")
+                        + encrypted.phrase.charAt(i) + System.getProperty("line.separator");
+
+                System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes(StandardCharsets.UTF_8)));
+                encrypted.guesses = game.enterLetter();
+                System.setIn(savedStandardInputStream);
+                game.currentSol();
+            }
+
+            }
+            assertTrue(testPlayer.getTotalGuesses() > 1);
+    }
+    /*@Test
+    public void TestLoadPlayer() throws IOException {
+        Player testPlayer = new Player(1, "hardCoded", 0.0, 5, 10, 1, 0);
+        Game game = new Game(testPlayer, "letter", "./src/test.txt");
+        System.setIn(System.in);
+        String simulatedUserInput2 = "hardCoded" + System.getProperty("line.separator");
+
+        System.setIn(new ByteArrayInputStream(simulatedUserInput2.getBytes(StandardCharsets.UTF_8)));
+        System.setIn(System.in);
+        Players.addPlayer();
+
+        Player testPlayer2 = Players.loadPlayer("hardCoded");
+        assertEquals(testPlayer.getCryptogramsCompleted(),testPlayer2.getCryptogramsCompleted());
+        }*/
 }
